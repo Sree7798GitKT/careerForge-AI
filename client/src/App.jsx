c02ddbe88560bb92f1f1bb493ac4ee9cb858abdd
Sreerag K T <sreeragkt7798@gmail.com>
2026-08-04 22:50:56 +0530

    Setup React frontend structure

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>CareerForge AI</h1>
+        <p>Build your career with AI.</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>CareerForge AI</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
